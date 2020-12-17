d4fd30b3b4e02413e422837b09840fb7028fa5cd
Vidyasagar Machupalli <vidyasagar.msc@in.ibm.com>
2020-12-17 16:52:03 +0530

    first commit

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+# Script to generate a secret
+generate_secret() {
+    PASS_LENGTH=20
+    local secret=`openssl rand -base64 48 | cut -c1-$PASS_LENGTH`
+    echo $secret
+}
+
+cat openshift.yaml | \
+  MYPROJECT=$MYPROJECT \
+  MYREGISTRY=$MYREGISTRY \
+  GIT_TOKEN_USERNAME=$GIT_TOKEN_USERNAME \
+  GIT_TOKEN_PASSWORD=$GIT_TOKEN_PASSWORD \
+  REPO_URL_WITHOUT_HTTPS=$REPO_URL_WITHOUT_HTTPS \
+  SECRET_GITHUB=$(generate_secret) \
+  SECRET_GENERIC=$(generate_secret) \
+  envsubst \
+  | \
+  oc apply -f - || exit 1
