833247108ad81819ca0a68d7ee5b34d1c2afd10e
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2021-08-02 17:47:54 +0530

    Changes to support new tutorial updates

--- generate_yaml.sh
+++ generate_yaml.sh
@@ -5,19 +5,7 @@ set -u
 set -o pipefail
 
 if [[ -z "$MYPROJECT" ]]; then
-  echo "Application name cannot be empty"
-fi
-
-if [[ -z "$MYREGISTRY" ]]; then
-  echo "Container registry cannot be empty"
-fi
-
-if [[ -z "$GIT_TOKEN_USERNAME" || -z "$GIT_TOKEN_PASSWORD" ]]; then
-  echo "Git(Private) token cannot be empty"
-fi
-
-if [[ -z "$REPO_URL_WITHOUT_HTTPS" ]]; then
-  echo "Git(Private) repo URL cannot be empty"
+    echo "Application name cannot be empty"
 fi
 
 # Script to generate a secret
@@ -27,25 +15,69 @@ generate_secret() {
     echo $secret
 }
 
-# Check for HTTPS and replace with empty string
-REPO_URL=${REPO_URL_WITHOUT_HTTPS/https:\/\//}
+use_private_registry() {
+    echo "Generating YAML for private container registry..."
+    if [[ -z "$MYREGISTRY" ]]; then
+        echo "Container registry cannot be empty"
+    fi
+    if [[ -z "$MYNAMESPACE" ]]; then
+        echo "Registry namespace cannot be empty"
+    fi
+    
+    GIT_REPO_URL="https://github.com/IBM-Cloud/openshift-node-app"
+    MYPROJECT="$MYPROJECT-registry"
+    
+    cat openshift.template.yaml | \
+    MYPROJECT=$MYPROJECT \
+    MYNAMESPACE=$MYNAMESPACE \
+    MYREGISTRY=$MYREGISTRY \
+    REPO_URL=$GIT_REPO_URL \
+    SECRET_GITHUB=$(generate_secret) \
+    SECRET_GENERIC=$(generate_secret) \
+    envsubst > openshift_private_registry.yaml
+    #| \
+    #oc apply -f - || exit 1
 
-# Check for `.git` in the end
-# Get the repo name
-REPO_NAME="$(cut -d'/' -f3 <<<"$REPO_URL")"
-SUB='.git'
-if [[ "$REPO_NAME" != *"$SUB"* ]]; then
-  REPO_URL=${REPO_URL}${SUB}
-fi
+    echo 'export MYPROJECT="$MYPROJECT-registry"'
+}
 
-cat openshift.template.yaml | \
-  MYPROJECT=$MYPROJECT \
-  MYREGISTRY=$MYREGISTRY \
-  GIT_TOKEN_USERNAME=$GIT_TOKEN_USERNAME \
-  GIT_TOKEN_PASSWORD=$GIT_TOKEN_PASSWORD \
-  REPO_URL_WITHOUT_HTTPS=$REPO_URL \
-  SECRET_GITHUB=$(generate_secret) \
-  SECRET_GENERIC=$(generate_secret) \
-  envsubst > openshift.yaml
-  #| \
-  #oc apply -f - || exit 1
+use_private_repository() {
+    
+    if [[ -z "$GIT_TOKEN_USERNAME" || -z "$GIT_TOKEN_PASSWORD" ]]; then
+        echo "Git(Private) token cannot be empty"
+    fi
+    
+    if [[ -z "$REPO_URL" ]]; then
+        echo "Git(Private) repo URL cannot be empty"
+    fi
+    # Check for HTTPS and replace with empty string
+    REPO_URL=${REPO_URL/https:\/\//}
+    
+    # Check for `.git` in the end
+    # Get the repo name
+    REPO_NAME="$(cut -d'/' -f3 <<<"$REPO_URL")"
+    SUB='.git'
+    if [[ "$REPO_NAME" != *"$SUB"* ]]; then
+        REPO_URL=${REPO_URL}${SUB}
+    fi
+    
+    MYPROJECT="$MYPROJECT-private-git"
+    GIT_REPO_URL="https://$GIT_TOKEN_USERNAME:$GIT_TOKEN_PASSWORD@$REPO_URL"
+
+    cat openshift.template.yaml | \
+    MYPROJECT=$MYPROJECT \
+    MYNAMESPACE=$MYNAMESPACE \
+    MYREGISTRY=$MYREGISTRY \
+    GIT_TOKEN_USERNAME=$GIT_TOKEN_USERNAME \
+    GIT_TOKEN_PASSWORD=$GIT_TOKEN_PASSWORD \
+    REPO_URL=$GIT_REPO_URL \
+    SECRET_GITHUB=$(generate_secret) \
+    SECRET_GENERIC=$(generate_secret) \
+    envsubst > openshift_private_repository.yaml
+    #| \
+    #oc apply -f - || exit 1
+
+    echo 'export MYPROJECT="$MYPROJECT-registry"'
+}
+
+$1
