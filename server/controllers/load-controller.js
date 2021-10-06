b5d120be637352f11204448dec67994c6b50bac1
Vidyasagar Machupalli <vidyasagar.msc@in.ibm.com>
2021-10-06 10:12:45 +0530

    Added load test endpoint

---
+++ server/controllers/load-controller.js
@@ -0,0 +1,17 @@
+// get health of application
+exports.getLoad = (req, res) => {
+    const load = req.params.load;
+    //console.log(load);
+    var value = 0;
+    while(value < load){
+        addDelay(value);
+        value++;
+    }
+  };
+  
+  function addDelay(value) {
+    setTimeout(function() {
+        console.log("Looping count:", value);
+        console.log("Current time in UTC:", new Date().toISOString());
+    }, 2000 * value)
+  }
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const healthRoutes = require('./routes/health-route');
 const swaggerRoutes = require('./routes/swagger-route');
+const LoadRoutes = require('./routes/load-route');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(bodyParser.json());
 // routes and api calls
 app.use('/health', healthRoutes);
 app.use('/swagger', swaggerRoutes);
+app.use('/load', LoadRoutes);
 
 // default path to serve up index.html (single page application)
 app.all('', (req, res) => {
@@ -26,6 +28,7 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App UI available http://localhost:${port}`);
   console.log(`Swagger UI available http://localhost:${port}/swagger/api-docs`);
+  console.log(`Generate load available http://localhost:${port}/load/10000`);
 });
 
 // error handler for unmatched routes or api calls
