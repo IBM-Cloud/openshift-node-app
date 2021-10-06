8f3ed93970b4987ef3edad545ff44c2e00b79119
Vidyasagar Machupalli <vidyasagar.msc@in.ibm.com>
2021-10-06 14:42:51 +0530

    Added sleep time

--- server/controllers/load-controller.js
+++ server/controllers/load-controller.js
@@ -1,18 +1,27 @@
 // get health of application
-exports.getLoad = (req, res) => {
-    const load = req.params.load;
-    req.setTimeout(0)
-    //console.log(load);
-    var value = 0;
-    while(value < load){
-        addDelay(value);
-        value++;
+exports.getLoad = (req, res, next) => {
+  const load = req.params.load;
+  res.setHeader("Content-Type", "text/html; charset=UTF-8");
+  res.writeHead(200);
+  req.setTimeout(0);
+  var value = 1;
+  async function addDelay() {
+    res.write("Total requests: " + load);
+    while (value < load) {
+      await sleep(3000);
+      //setTimeout(function() {
+      //console.log("Looping count:", value);
+      console.log("Current time in UTC:", new Date().toISOString());
+      res.write(
+        "<p>" + "Response#" + value + " at " + new Date().toISOString() + "</p>"
+      );
+      value++;
     }
-  };
-  
-  function addDelay(value) {
-    setTimeout(function() {
-        console.log("Looping count:", value);
-        console.log("Current time in UTC:", new Date().toISOString());
-    }, 2000 * value)
+    res.end();
   }
+  addDelay();
+};
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
